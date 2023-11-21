4f12f88fd795128bd1d261f60fe0653404653b3f
N3v1 <n3v1010@gmail.com>
2023-11-21 20:03:28 +0100

    ✨ More: SLButton & SLImageButton
    - New Metal shaders
    - SLButton & SLImageButton
    - Tutorial Guide: new VM
    - Added new Image in Asset catalogue

---
+++ ScribbleLab/Core/Componets/Views/SLButton.swift
@@ -0,0 +1,95 @@
+//
+//  SLButton.swift
+//  ScribbleLab
+//
+//  Created by Nevio Hirani on 21.11.23.
+//
+
+import SwiftUI
+
+/// A customizable button view.
+///
+/// This view creates a styled button with customizable appearance and action.
+///
+/// Example Usage:
+/// ```swift
+/// SLButton(text: String, 
+/// font: Font,
+/// backgroundColor: Color,
+/// textColor: Color,
+/// cornerRadius: CGFloat,
+/// action: @escaping () -> Void)
+/// ```
+struct SLButtonView: View {
+    var text: String
+    var font: Font
+    var backgroundColor: Color
+    var textColor: Color
+    var cornerRadius: CGFloat
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text(text)
+                .font(font) // .subheadline
+                .fontWeight(.semibold)
+                .foregroundColor(textColor) // .black
+                .frame(width: 360, height: 44)
+                .background(backgroundColor)
+                .cornerRadius(cornerRadius) // 10
+                // MARK: -  Stroke of the Button (if backgroundColor = .clear):
+                .background {
+                    RoundedRectangle(cornerRadius: 13)
+                        .fill(Color(red: 248/255, green: 248/255, blue: 248/255))
+                        .strokeBorder(Color(red: 194/255, green: 194/255, blue: 194/255), lineWidth: 0.5)
+                }
+        }
+    }
+}
+
+extension View {
+    /// Creates a styled button with customizable appearance and action.
+    ///
+    /// Use this function to create a button with specific text, font, colors, corner radius, and an associated action.
+    ///
+    /// - Parameters:
+    ///   - text: The text to display on the button.
+    ///   - font: The font to apply to the button text.
+    ///   - color: The background color of the button.
+    ///   - textColor: The color of the button's text.
+    ///   - cornerRadius: The corner radius of the button.
+    ///   - action: The action to perform when the button is tapped.
+    ///
+    /// - Returns: A view representing the styled button.
+    /// 
+    func SLButton(text: String, font: Font, backgroundColor: Color, textColor: Color, cornerRadius: CGFloat, action: @escaping () -> Void) -> some View {
+        SLButtonView(text: text, font: font, backgroundColor: backgroundColor, textColor: textColor, cornerRadius: cornerRadius, action: action)
+    }
+}
+
+// MARK: - SLButton Test View
+/// A test view for the ```SLButton```'s
+///
+/// Use this view to test the SLButtonView functionality.
+/// This view provides examples of using the `SLButton` with different configurations.
+///
+struct SLButtonTestView: View {
+    var body: some View {
+        VStack {
+            Text("This is a View to test the SLButton!")
+            
+            SLButton(text: "Tab on me", font: .subheadline, backgroundColor: .orange, textColor: .black, cornerRadius: 10) {
+                print("Button was pressed!")
+            }
+            SLButton(text: "Tab on me", font: .subheadline, backgroundColor: .clear, textColor: .black, cornerRadius: 10) {
+                print("Button was pressed!")
+            }
+        }
+    }
+}
+
+#Preview {
+    SLButtonTestView()
+}
---
+++ ScribbleLab/Core/Tour/Model/PageIntro.swift
@@ -0,0 +1,25 @@
+//
+//  PageIntro.swift
+//  ScribbleLab
+//
+//  Created by Nevio Hirani on 18.11.23.
+//
+
+import SwiftUI
+
+/// Page Intro Model
+struct PageIntro: Identifiable, Hashable {
+    var id: UUID = .init()
+    var introAssetImage: String
+    var title: String
+    var subtitle: String
+    var description: String
+    var displayAction: Bool = false
+}
+
+//var pageIntro: [PageIntro] = [
+//    .init(introAssetImage: "people-intro", title: "Welcome to ScribbleLab", subtitle: "Start your note-taking journey by following this Tutorial", description: "To become a ScribbleLab Pro follow this Tutorial in order to become more familiar with ScribbleLab.", displayAction: <#T##Bool#>),
+//    .init(introAssetImage: "person-illustartion-grade", title: "Explore our Editor functionalities", subtitle: "", description: <#String#>, displayAction: <#T##Bool#>),
+//    .init(introAssetImage: <#T##String#>, title: <#T##String#>, subtitle: <#T##String#>, description: <#String#>, displayAction: <#T##Bool#>),
+//    .init(introAssetImage: <#T##String#>, title: <#T##String#>, subtitle: <#T##String#>, description: <#String#>, displayAction: <#T##Bool#>)
+//]
