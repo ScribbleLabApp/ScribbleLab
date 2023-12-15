e0d9afaf9f6aaffb920b4cb7247244a26302fcae
N3v1 <n3v1010@gmail.com>
2023-12-15 21:36:59 +0100

    Adding macOS target, PrivacyInfo

---
+++ ScribbleLab MacOS/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  ScribbleLab MacOS
+//
+//  Created by Nevio Hirani on 15.12.23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+#Preview {
+    ContentView()
+}
