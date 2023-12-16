84b7ec69c58d124e9ebfc9f25ba510aaa7c8aec2
N3v1 <n3v1010@gmail.com>
2023-12-16 21:28:24 +0100

    [CI/CD]: Investigate CI-test workflow

--- ScribbleLab/Core/Settings/View/Help/View/SLHelpView.swift
+++ ScribbleLab/Core/Settings/View/Help/View/SLHelpView.swift
@@ -22,20 +22,19 @@ struct SLHelpView: View {
                     } label: {
                         Label("Report a bug",systemImage: "ladybug")
                     }
-//                    .onTapGesture {
-//                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml&title=%F0%9F%94%B4+%3Cbug+name%3E")!)
-//                    }
                     .tint(.black)
+                    
                     Button {
-                        
+                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=crash&projects=&template=crash_report.yml&title=%F0%9F%A7%A8+%3Ccrash+report%3E")!)
                     } label: {
                         Label("Report a crash", systemImage: "laptopcomputer.trianglebadge.exclamationmark")
                     }
                     .tint(.black)
+                    
                     Button {
-                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml&title=%F0%9F%94%B4+%3Cbug+name%3E")!)
+                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=translation%2C+good-first-issue%2C+bug&projects=&template=translation_issue.yml&title=%F0%9F%8C%8E+Translation+Issue+in+%3CScreen%3E")!)
                     } label: {
-                        Label("Report a translation issue", systemImage: "character.bubble.ja") // bubble.left.and.exclamationmark.bubble.right
+                        Label("Report a translation issue", systemImage: "character.bubble.ja")
                     }
                     .tint(.black)
                 } header: {
@@ -45,11 +44,36 @@ struct SLHelpView: View {
                 }
                 
                 Section() {
-                    Label("Ask a question", systemImage: "questionmark.bubble")
-                    Label("Q&A", systemImage: "bubble.left.and.text.bubble.right.rtl")
-                    Label("Documentation", systemImage: "book.and.wrench") // books.vertical
+                    Button {
+                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/discussions/categories/q-a")!)
+                    } label: {
+                        Label("Ask a question", systemImage: "questionmark.bubble")
+                    }
+                    .tint(.black)
+                    
                     Button {
                         
+                    } label: {
+                        Label("Join our Discord Community", systemImage: "server.rack")
+                    }
+                    .tint(.black)
+                    
+                    Button {
+                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/discussions/categories/q-a")!)
+                    } label: {
+                        Label("Q&A", systemImage: "bubble.left.and.text.bubble.right.rtl")
+                    }
+                    .tint(.black)
+                    
+                    Button {
+                        
+                    } label: {
+                        Label("Documentation", systemImage: "book.and.wrench")
+                    }
+                    .tint(.black)
+                    
+                    Button {
+                        openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab")!)
                     } label: {
                         Label("Guide", systemImage: "character.book.closed")
                     }
@@ -62,7 +86,7 @@ struct SLHelpView: View {
                 
                 Section() {
                     NavigationLink {
-                        
+                        SLTroubleshootingView()
                     } label: {
                         Label("Troubleshooting", systemImage: "exclamationmark.triangle")
                     }
@@ -82,8 +106,8 @@ struct SLHelpView: View {
                     Text("When Safe Mode is enabled, ScribbleLab starts with several features disabled to ensure maximum stability or detect errors. This process requires a restart.")
                 }
             }
-                .navigationTitle("Help")
-                .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Help")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
