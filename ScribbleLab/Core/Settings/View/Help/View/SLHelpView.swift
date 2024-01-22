c3be2d1da73c477301d5a132c9c9031f72c73104
N3v1 <n3v1010@gmail.com>
2024-01-22 19:21:22 +0100

    [Chore]: HelpView Darkmode support

--- ScribbleLab/Core/Settings/View/Help/View/SLHelpView.swift
+++ ScribbleLab/Core/Settings/View/Help/View/SLHelpView.swift
@@ -24,21 +24,21 @@ struct SLHelpView: View {
                     } label: {
                         Label("Report a bug",systemImage: "ladybug")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=crash&projects=&template=crash_report.yml&title=%F0%9F%A7%A8+%3Ccrash+report%3E")!)
                     } label: {
                         Label("Report a crash", systemImage: "laptopcomputer.trianglebadge.exclamationmark")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/issues/new?assignees=&labels=translation%2C+good-first-issue%2C+bug&projects=&template=translation_issue.yml&title=%F0%9F%8C%8E+Translation+Issue+in+%3CScreen%3E")!)
                     } label: {
                         Label("Report a translation issue", systemImage: "character.bubble.ja")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                 } header: {
                     Text("Report a bug")
                 } footer: {
@@ -51,35 +51,35 @@ struct SLHelpView: View {
                     } label: {
                         Label("Ask a question", systemImage: "questionmark.bubble")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         
                     } label: {
                         Label("Join our Discord Community", systemImage: "server.rack")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab/discussions/categories/q-a")!)
                     } label: {
                         Label("Q&A", systemImage: "bubble.left.and.text.bubble.right.rtl")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         
                     } label: {
                         Label("Documentation", systemImage: "book.and.wrench")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                     
                     Button {
                         openURL(URL(string: "https://github.com/ScribbleLabApp/ScribbleLab")!)
                     } label: {
                         Label("Guide", systemImage: "character.book.closed")
                     }
-                    .tint(.black)
+//                    .tint(.orange)
                 } header: {
                     Text("Help")
                 } footer: {
@@ -108,6 +108,7 @@ struct SLHelpView: View {
                     Text("When Safe Mode is enabled, ScribbleLab starts with several features disabled to ensure maximum stability or detect errors. This process requires a restart.")
                 }
             }
+            .tint(.orange)
             .navigationTitle("Help")
             .navigationBarTitleDisplayMode(.inline)
         }
