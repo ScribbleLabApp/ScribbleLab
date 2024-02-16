607975d7f08aac12405b61014088d4b25a4dd06e
N3v1 <n3v1010@gmail.com>
2024-02-16 20:04:14 +0100

    [Chore]: Add Notification Onboarding

--- ScribbleLab/Notifications/Service/UI/AskNotificationView.swift
+++ ScribbleLab/Notifications/Service/UI/AskNotificationView.swift
@@ -10,14 +10,70 @@ import SwiftUI
 struct AskNotificationView: View {
     var body: some View {
         VStack {
-            Image(.notification)
-                .ignoresSafeArea()
+            Spacer()
             
-            Image(.memoji)
+            VStack(spacing: -5) {
+                Image("notification-iPad")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 500)
+                
+                Image(.memoji)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 550)
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 10)
+            
+            Text("Don't miss out on what your \nfriends are up to")
+                .font(.title)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 5)
+            
+            Text("Never miss those precious moments.")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            
+            VStack(spacing: 25) {
+                Button {
+                    
+                } label: {
+                    Text("Turn on notifications")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .tint(.white)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background {
+                            Capsule()
+                                .foregroundStyle(.orange)
+                        }
+                }
+                
+                
+                Button {
+                    
+                } label: {
+                    Text("Another time")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .tint(.secondary)
+                }
+            }
+            .padding(.vertical, 30)
+            .padding(.horizontal, 10)
+            
+            Spacer()
         }
     }
 }
 
 #Preview {
-    AskNotificationView()
+    Text("Hi")
+        .sheet(isPresented: .constant(true)) {
+            AskNotificationView()
+        }
 }
