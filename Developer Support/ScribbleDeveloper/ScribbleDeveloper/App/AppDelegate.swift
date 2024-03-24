ee6049c64fca6f357fd7d24d67b691e7723a1a83
N3v1 <n3v1010@gmail.com>
2024-03-24 15:51:45 +0100

    [ScribbleLab]: Update package dependencies

---
+++ Developer Support/ScribbleDeveloper/ScribbleDeveloper/App/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  ScribbleDeveloper
+//
+//  Created by Nevio Hirani on 24.03.24.
+//
+
+import SwiftUI
+import Combine
+
+final class ScribbleDeveloperApplication: NSApplication {
+    let strongDelegate = AppDelegate()
+
+    override init() {
+        super.init()
+        self.delegate = strongDelegate
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+@NSApplicationMain
+final class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+    func applicationWillFinishLaunching(_ notification: Notification) {}
+    
+    var statusItem: NSStatusItem?
+    
+    
+}
